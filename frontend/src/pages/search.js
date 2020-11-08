53e85201612a0b0213374a29ba4b8e4ad008e47d
Marc Lewis <marc8@live.unc.edu>
2020-11-07 19:45:49 -0500

    Added initial search page, linked to youtube API with key

---
+++ frontend/src/pages/search.js
@@ -0,0 +1,62 @@
+import Link from 'next/link';
+import { Form, Input, Button, Checkbox } from 'antd';
+
+const axios = require('axios');
+// https://developers.google.com/youtube/v3/docs/search/list -YT API
+
+function Search() {
+  return (
+    <div>
+      <p> Hello There </p>
+      <SearchBox></SearchBox>
+    </div>
+  );
+}
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
+
+function SearchBox() {
+  const onFinish = async (values) => {
+    let result = await axios({
+      method: 'get',
+      url: `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${values.name}&type=video&key=AIzaSyBcWEFvenf9oWCPuhMboUCeCvbt3kPbntU`,
+    });
+    console.log(result);
+    // TODO, actually build search results into visible data
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+  return (
+    <Form
+      {...layout}
+      name="basic"
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <Form.Item
+        label="Search"
+        name="name"
+        rules={[{ required: true, message: 'Fill in this field' }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+}
+
+export default Search;
