1a3793db79767aea0c40eed40b376cebcfb8e45f
Kush Patel <kkpatel4@live.unc.edu>
2020-11-21 21:07:44 -0500

    Fix home page and navigation

--- frontend/src/pages/auth.js
+++ frontend/src/pages/auth.js
@@ -5,6 +5,7 @@ import { useRouter } from 'next/router';
 import { setCookie, parseCookies, destroyCookie } from 'nookies';
 import { Button } from 'antd';
 import { deezerAuth, fetchDeezerToken } from '../endpoints';
+import { Footer, Navbar } from '../components';
 import _ from 'lodash';
 
 function SpotifyLaunch() {
@@ -21,53 +22,59 @@ function SpotifyLaunch() {
     deezerToken = null;
   }
 
-  return spotifyToken ? (
-    deezerToken ? (
-      <div>
-        <h1>You are authenticated with Spotify and Deezer!</h1>
-        <Link href="./search">
-          <Button>Start Searching!</Button>
-        </Link>
-      </div>
-    ) : (
-      <div>
-        <h1>You are authenticated with Spotify!</h1>
-        <Button
-          onClick={async (e) => {
-            let headers = await deezerAuth();
-            router.push(headers['x-final-url']);
-            let res = await fetchDeezerToken(router.query.code);
-            setCookie(null, 'deezerAuthToken', res.access_token, {
-              maxAge: 60 * 60 * 1000,
-            });
-          }}
-        >
-          Authenticate with Deezer
-        </Button>
-        <Link href="./search">
-          <Button>Start Searching!</Button>
-        </Link>
-      </div>
-    )
-  ) : (
+  return (
     <div>
-      <SpotifyAuth
-        redirectUri="http://localhost:3000/auth"
-        clientID={process.env.spotifyClientID}
-        scopes={[Scopes.userReadPrivate, 'user-read-email']} // either style will work
-      />
-      <Button
-        onClick={async (e) => {
-          let headers = await deezerAuth();
-          router.push(headers['x-final-url']);
-          let res = await fetchDeezerToken(router.query.code);
-          setCookie(null, 'deezerAuthToken', res.access_token, {
-            maxAge: 60 * 60 * 1000,
-          });
-        }}
-      >
-        Authenticate with Deezer
-      </Button>
+      <Navbar />
+      {spotifyToken ? (
+        deezerToken ? (
+          <div>
+            <h2>You are authenticated with Spotify and Deezer!</h2>
+            <Link href="./search">
+              <Button>Start Searching!</Button>
+            </Link>
+          </div>
+        ) : (
+          <div>
+            <h1>You are authenticated with Spotify!</h1>
+            <Button
+              onClick={async (e) => {
+                let headers = await deezerAuth();
+                router.push(headers['x-final-url']);
+                let res = await fetchDeezerToken(router.query.code);
+                setCookie(null, 'deezerAuthToken', res.access_token, {
+                  maxAge: 60 * 60 * 1000,
+                });
+              }}
+            >
+              Authenticate with Deezer
+            </Button>
+            <Link href="./search">
+              <Button>Start Searching!</Button>
+            </Link>
+          </div>
+        )
+      ) : (
+        <div>
+          <SpotifyAuth
+            redirectUri="http://localhost:3000/auth"
+            clientID={process.env.spotifyClientID}
+            scopes={[Scopes.userReadPrivate, 'user-read-email']} // either style will work
+          />
+          <Button
+            onClick={async (e) => {
+              let headers = await deezerAuth();
+              router.push(headers['x-final-url']);
+              let res = await fetchDeezerToken(router.query.code);
+              setCookie(null, 'deezerAuthToken', res.access_token, {
+                maxAge: 60 * 60 * 1000,
+              });
+            }}
+          >
+            Authenticate with Deezer
+          </Button>
+        </div>
+      )}
+      <Footer />
     </div>
   );
 }
--- frontend/src/pages/signed-in.js
+++ frontend/src/pages/signed-in.js
@@ -5,12 +5,14 @@ function SignedIn() {
   return (
     <div>
       <h1 id="title">FlexDJ</h1>
-      <h2>
-        Explore your favorite music from Spotify, YouTube, and SoundCloud!
-      </h2>
-      <div>
-        <SearchBox />
-      </div>
+      <h2>Explore your favorite music from Spotify, YouTube, and Deezer!</h2>
+      <Link href="./search">
+        <button>Start Searching!</button>
+      </Link>
+      <br></br>
+      <br></br>
+      <br></br>
+      <h3>To check or add authentication for Spotify and Deezer:</h3>
       <Link href="./auth">
         <button type="submit">Authenticate</button>
       </Link>
