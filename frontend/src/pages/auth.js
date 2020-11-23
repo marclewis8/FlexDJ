12d683b3c458650d6a597680208a9081fe4d36bd
chiazo <chiazo@live.unc.edu>
2020-11-22 19:46:06 -0500

    change backend port

--- frontend/src/pages/auth.js
+++ frontend/src/pages/auth.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Link from 'next/link';
 import { SpotifyAuth, Scopes } from 'react-spotify-auth';
 import { useRouter } from 'next/router';
